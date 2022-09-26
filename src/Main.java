3e4457015d696981da62331a30c589c639345523
NichlasMau <Nich5384@stud.kea.dk>
2022-09-26 09:50:54 +0200

    oprettet raflebæger projekt oprettet klasser og objekter

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Raflebæger raflebæger = new Raflebæger(2);
+
+        int samletAntalØjne = raflebæger.ryst();
+        System.out.println(samletAntalØjne);
+    }
+
+}
---
+++ src/Raflebæger.java
@@ -0,0 +1,11 @@
+public class Raflebæger {
+    private int antalTerninger;
+    public Raflebæger(int antalTerninger){
+        this.antalTerninger = antalTerninger;
+    }
+
+    public int ryst(){
+        //simulere ryst med to "terninger"
+      return 10;
+    }
+}
